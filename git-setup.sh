6c6963496c84cff84df45a02558a766c9acafe0d
Bo-Yi Lee <boyil@qca.qualcomm.com>
2016-10-29 22:15:45 -0700

    otp_test.sh can successfully read-write OTP bank0

---
+++ git-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "# ROME-OTP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/qcm/ROME-OTP.git
+git push -u origin master
+
